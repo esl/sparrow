3781e2fbec308ff50d1d22e1a0af24452deafdfe
zofpolkowska <zofpolkowska@gmail.com>
2019-11-19 15:27:47 +0100

    Add name in H2Worker state

--- lib/sparrow/h2_worker/state.ex
+++ lib/sparrow/h2_worker/state.ex
@@ -1,17 +1,20 @@
 defmodule Sparrow.H2Worker.State do
   @moduledoc false
   @type connection_ref :: pid
+  @type name :: :disconnected | :connected
   @type stream_id :: non_neg_integer
   @type requests :: %{required(stream_id) => %Sparrow.H2Worker.Request{}}
   @type config :: %Sparrow.H2Worker.Config{}
 
   @type t :: %__MODULE__{
+          name: name,
           connection_ref: connection_ref | nil,
           requests: requests,
           config: config
         }
 
   defstruct [
+    :name,
     :connection_ref,
     :requests,
     :config
@@ -21,8 +24,20 @@ defmodule Sparrow.H2Worker.State do
   Creates new empty `Sparrow.H2Worker.State`.
   """
   @spec new(connection_ref | nil, requests, config) :: t
-  def new(connection_ref, requests \\ %{}, config) do
+  def new(connection_ref, requests \\ %{}, config)
+
+  def new(nil, requests, config) do
     %__MODULE__{
+      name: :disconnected,
+      connection_ref: nil,
+      requests: requests,
+      config: config
+    }
+  end
+
+  def new(connection_ref, requests, config) do
+    %__MODULE__{
+      name: :connected,
       connection_ref: connection_ref,
       requests: requests,
       config: config
@@ -35,6 +50,7 @@ defmodule Sparrow.H2Worker.State do
   @spec reset_requests_collection(t) :: t
   def reset_requests_collection(state) do
     %__MODULE__{
+      name: state.name,
       connection_ref: state.connection_ref,
       requests: %{},
       config: state.config
@@ -47,6 +63,7 @@ defmodule Sparrow.H2Worker.State do
   @spec reset_connection_ref(t) :: t
   def reset_connection_ref(state) do
     %__MODULE__{
+      name: :disconnected,
       connection_ref: nil,
       requests: state.requests,
       config: state.config
