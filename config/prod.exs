76033f1a9f952d5215abd4d0c95fea3cdf752e56
Rafał Słota <rafal.slota@erlang-solutions.com>
2019-11-26 17:03:20 +0100

    Add clean config/prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -1,85 +1,14 @@
 use Mix.Config
 
+# Please refer to README.md to get more detail of how this file should look like.
+# You can also look @ `config/prod.example.exs` to get rough idea how this should work.
+
 config :sparrow,
   fcm: [
-    # Authentication
-    path_to_json: "priv/fcm/token/sparrow_token.json",
-    # H2Worker config
-    endpoint: "fcm.googleapis.com",
-    port: 443,
-    tls_opts: [],
-    ping_interval: 5000,
-    reconnect_attempts: 3,
-    # pool config
-    tags: [],
-    worker_num: 3,
-    raw_opts: []
-  ],
+    # Your FCM pool definitions here
+  ]
+
+config :sparrow,
   apns: [
-    dev: [
-      [
-        # Token based authentication
-        auth_type: :token_based,
-        token_id: :some_atom_id,
-        # H2Worker config
-        endpoint: "api.development.push.apple.com",
-        port: 443,
-        tls_opts: [],
-        ping_interval: 5000,
-        reconnect_attempts: 3,
-        # pool config
-        tags: [:first_batch_clients, :beta_users],
-        worker_num: 3,
-        raw_opts: []
-      ],
-      [
-        # Certificate based uthentication
-        auth_type: :certificate_based,
-        cert: "priv/apns/dev_cert.pem",
-        key: "priv/apns/dev_key.pem",
-        # H2Worker config
-        endpoint: "api.push.apple.com",
-        port: 443,
-        tls_opts: [],
-        ping_interval: 5000,
-        reconnect_attempts: 3,
-        # pool config
-        tags: [:another_batch_clients],
-        worker_num: 3,
-        raw_opts: []
-      ]
-    ],
-    prod: [
-      [
-        # Authentication
-        auth_type: :token_based,
-        token_id: :some_other_id,
-        # H2Worker config
-        endpoint: "api.push.apple.com",
-        port: 443,
-        tls_opts: [],
-        ping_interval: 5000,
-        reconnect_attempts: 3,
-        # pool config
-        tags: [:test_prod, :alpha],
-        worker_num: 3,
-        raw_opts: []
-      ]
-    ],
-    tokens: [
-      [
-        # TODO replace me with real data
-        token_id: :some_atom_id,
-        key_id: "FAKE_KEY_ID",
-        team_id: "FAKE_TEAM_ID",
-        p8_file_path: "token.p8"
-      ],
-      [
-        # TODO replace me with real data
-        token_id: :some_other_id,
-        key_id: "FAKE_KEY_ID",
-        team_id: "FAKE_TEAM_ID",
-        p8_file_path: "token.p8"
-      ]
-    ]
+    # Your FCM pool definitions here
   ]
