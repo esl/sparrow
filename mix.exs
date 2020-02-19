4d6a2060a9b936f61ce0a689b6dd9685574bd7a4
Rafał Słota <rafal.slota@erlang-solutions.com>
2020-02-19 16:21:55 +0100

    Fix FCM error codes

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sparrow.MixProject do
   def project do
     [
       app: :sparrow,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
