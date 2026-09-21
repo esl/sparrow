3a3e3da7a06e69116bf036d8f3e1c629c41b56ad
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2026-09-21 10:17:29 +0200

    Move the map comparison to the function head

--- lib/sparrow/util.ex
+++ lib/sparrow/util.ex
@@ -2,10 +2,11 @@ defmodule Sparrow.Util do
   @moduledoc "Utilities for building notifications"
 
   @spec add_if_not_empty(map, term, map) :: map
+  def add_if_not_empty(map, _key, sub_map) when map_size(sub_map) == 0 do
+    map
+  end
+
   def add_if_not_empty(map, key, sub_map) do
-    case sub_map == %{} do
-      true -> map
-      false -> Map.put(map, key, sub_map)
-    end
+    Map.put(map, key, sub_map)
   end
 end
